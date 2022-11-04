592579b9ce59fe42158df3b80d8f2df310d3727c
Christian Pichardo <christianpichardo@wireless-10-146-206-195.public.utexas.edu>
2022-11-04 15:54:17 -0500

    Added more resource files for the nav bar

--- app/src/main/java/com/example/adlunam/ui/images/ImagesFragment.kt
+++ app/src/main/java/com/example/adlunam/ui/images/ImagesFragment.kt
@@ -1,4 +1,4 @@
-package com.example.adlunam.ui.dashboard
+package com.example.adlunam.ui.images
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.adlunam.databinding.FragmentDashboardBinding
 
-class DashboardFragment : Fragment() {
+class ImagesFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
@@ -22,14 +22,14 @@ class DashboardFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(DashboardViewModel::class.java)
+        val imagesViewModel =
+            ViewModelProvider(this).get(ImagesViewModel::class.java)
 
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
+        imagesViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/example/adlunam/ui/images/ImagesViewModel.kt
+++ app/src/main/java/com/example/adlunam/ui/images/ImagesViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.adlunam.ui.dashboard
+package com.example.adlunam.ui.images
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ImagesViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "This is images Fragment"
     }
     val text: LiveData<String> = _text
 }
